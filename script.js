e8580a11d487b387819b0d675c8e9d4460178754
rohit kumar <rohitkumar38344@gmail.com>
2023-03-01 01:10:27 +0530

    added functionality to use hexadecimal color format

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-document
-  .querySelector(".changeBgColorBtn")
-  .addEventListener("click", changeColor);
-
-function changeColor() {
+// Generates Random color for rgb color format
+const randomColorRGB = () => {
   let r = Math.trunc(Math.random() * 255 + 1);
   let g = Math.trunc(Math.random() * 255 + 1);
   let b = Math.trunc(Math.random() * 255 + 1);
 
-  document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+  return `rgb(${r},${g},${b})`;
+};
+
+// Generates Random color for Hexadecimal color format
+const randomColorHex = () => {
+  const hexString = "0123456789ABCDEF";
+  let prefix = "#";
+  for (let i = 0; i < 6; i++) {
+    prefix += hexString[Math.floor(Math.random() * 16)];
+  }
+
+  return prefix;
+};
+
+// changing background color of the body
+function changeColor() {
+  document.body.style.backgroundColor = randomColorRGB();
 }
+
+// adding event listener to button
+document
+  .querySelector(".changeBgColorBtn")
+  .addEventListener("click", changeColor);
