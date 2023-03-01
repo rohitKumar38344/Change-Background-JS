d37c0f2ff2e144e3faa720ee629773d595d64c08
rohit kumar <rohitkumar38344@gmail.com>
2023-03-02 00:55:27 +0530

    v2

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
+let hexCode = "#2211AB"; //default value
+let rgbCode = "rgb(23, 42, 88)"; // default value
+const outputSection = document.querySelector("h2");
+
 // Generates Random color for rgb color format
-const randomColorRGB = () => {
+const randomRGBColor = () => {
   let r = Math.trunc(Math.random() * 255 + 1);
   let g = Math.trunc(Math.random() * 255 + 1);
   let b = Math.trunc(Math.random() * 255 + 1);
 
-  return `rgb(${r},${g},${b})`;
+  rgbCode = `rgb(${r},${g},${b})`;
+  return rgbCode;
 };
 
 // Generates Random color for Hexadecimal color format
-const randomColorHex = () => {
+const randomHexColor = () => {
   const hexString = "0123456789ABCDEF";
   let prefix = "#";
   for (let i = 0; i < 6; i++) {
     prefix += hexString[Math.floor(Math.random() * 16)];
   }
-
-  return prefix;
+  hexCode = prefix;
+  return hexCode;
 };
 
-// changing background color of the body
-function changeColor() {
-  document.body.style.backgroundColor = randomColorRGB();
+// Applying RGB background color to the body
+function changeRGBColor() {
+  document.body.style.backgroundColor = randomRGBColor();
+  outputSection.innerText = rgbCode;
+}
+
+// Applying Hexadecimal background color to the body
+function changeHexColor() {
+  document.body.style.backgroundColor = randomHexColor();
+  outputSection.innerText = hexCode;
 }
 
 // adding event listener to button
-document
-  .querySelector(".changeBgColorBtn")
-  .addEventListener("click", changeColor);
+document.querySelector(".hexBtn").addEventListener("click", changeHexColor);
+
+document.querySelector(".rgbBtn").addEventListener("click", changeRGBColor);
